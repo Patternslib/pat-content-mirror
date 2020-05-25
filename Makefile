974ac1d94fcd7265a4eaf8500ab93f88f451c0f7
Johannes Raggam <thetetet@gmail.com>
2020-05-25 20:12:21 +0200

    Update package and build infrastructure

--- Makefile
+++ Makefile
@@ -1,16 +1,15 @@
 all:: designerhappy
 
-########################################################################
-## Install dependencies
 
 stamp-npm: package.json
 	npm install
 	touch stamp-npm
+
 
 clean::
 	rm -rf stamp-npm node_modules
 
 
 designerhappy:: stamp-npm
-	printf "\n\n Designer, you can be happy now.\n Go to http://localhost:8080 to see the demo \n\n\n\n"
+	printf "\n\n Designer, you can be happy now.\n Go to http://localhost:8000 to see the demo \n\n\n\n"
 	npm run start
--- bundle-config.js
+++ bundle-config.js
@@ -1,19 +1,5 @@
-define([
-   "jquery",
-   "pat-registry",
-   "pat-content-mirror", 
-   "jquery.browser"
-], function($, registry) {
-    // Since we are in a non-AMD env, register a few useful utilites
-    var window = require("window");
-    window.jQuery = $;
-    require("imports-loader?this=>window!jquery.browser");
-    // if ($.browser.msie) {
-    //     $("#settings").remove();
-    // }
-    $(function () {
-        registry.init();
-    });
-    return registry;
-});
+import registry from "patternslib/src/core/registry.js";
+import "./src/pat-content-mirror.js";
 
+
+registry.init();
