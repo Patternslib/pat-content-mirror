1c9abbb0fd7c9be58a399df5450bf5de092ce1aa
Johannes Raggam <thetetet@gmail.com>
2020-09-30 11:53:01 +0200

    prettier

--- src/pat-content-mirror.js
+++ src/pat-content-mirror.js
@@ -6,38 +6,45 @@ import Parser from "patternslib/src/core/parser";
 const parser = new Parser("content-mirror");
 parser.add_argument("target");
 
-
 export default Base.extend({
-  name: "content-mirror",
-  trigger: ".pat-content-mirror",
-  defaults: {
-    target: "p.content-mirror .text",
-  },
+    name: "content-mirror",
+    trigger: ".pat-content-mirror",
+    defaults: {
+        target: "p.content-mirror .text",
+    },
 
-  init: function content_mirror_init($el, opts) {
-    const options = parser.parse($el, opts, true)[0];
-    this.options = _.extend(_.clone(this.defaults), options);
-    const $mirror = $(this.options.target).parents("p.content-mirror").first();
-    $el.on("input propertychange", $.proxy(this.updateMirror, this, this.options.target));
-    $el
-      .parents("form")
-      .first()
-      .on("reset", function (e) {
-        $el.val("");
-        $mirror.html($mirror.html());
-      });
-    $(".placeholder", this.options.target).text($el.attr("placeholder") || "");
-  },
+    init: function content_mirror_init($el, opts) {
+        const options = parser.parse($el, opts, true)[0];
+        this.options = _.extend(_.clone(this.defaults), options);
+        const $mirror = $(this.options.target)
+            .parents("p.content-mirror")
+            .first();
+        $el.on(
+            "input propertychange",
+            $.proxy(this.updateMirror, this, this.options.target)
+        );
+        $el.parents("form")
+            .first()
+            .on("reset", function (e) {
+                $el.val("");
+                $mirror.html($mirror.html());
+            });
+        $(".placeholder", this.options.target).text(
+            $el.attr("placeholder") || ""
+        );
+    },
 
-  updateMirror: function updateMirror(target, ev) {
-    const $el = $(ev.target);
-    const the_mirror = $(target);
-    the_mirror.text($el.val());
-    if (!$el.val().length) {
-      const placeholder = $el.attr("placeholder");
-      if (placeholder) {
-        the_mirror.html('<em class="placeholder">' + placeholder + "</em>");
-      }
-    }
-  },
+    updateMirror: function updateMirror(target, ev) {
+        const $el = $(ev.target);
+        const the_mirror = $(target);
+        the_mirror.text($el.val());
+        if (!$el.val().length) {
+            const placeholder = $el.attr("placeholder");
+            if (placeholder) {
+                the_mirror.html(
+                    '<em class="placeholder">' + placeholder + "</em>"
+                );
+            }
+        }
+    },
 });
