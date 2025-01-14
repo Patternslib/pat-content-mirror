78c3b678f4b6fd80e1dc4dea73159ce21f0a8549
Johannes Raggam <thetetet@gmail.com>
2025-01-14 20:07:10 +0100

    maint(pat-content-mirror): Rework for class based pattern.
    Note: the event binding on the propertychange event is removed as this dates
    back to IE times and is not supported by any modern browser.

--- src/pat-content-mirror.js
+++ src/pat-content-mirror.js
@@ -1,39 +1,57 @@
-import $ from "jquery";
-import Base from "@patternslib/patternslib/src/core/base";
+import { BasePattern } from "@patternslib/patternslib/src/core/basepattern";
+import events from "@patternslib/patternslib/src/core/events";
 import Parser from "@patternslib/patternslib/src/core/parser";
+import registry from "@patternslib/patternslib/src/core/registry";
 
 const parser = new Parser("content-mirror");
 parser.addArgument("target", "p.content-mirror .text");
 
-export default Base.extend({
-    name: "content-mirror",
-    trigger: ".pat-content-mirror",
+class Pattern extends BasePattern {
+    static name = "content-mirror";
+    static trigger = ".pat-content-mirror";
+    static parser = parser;
 
-    init: function content_mirror_init($el) {
-        this.options = parser.parse(this.el, this.options);
-        const $mirror = $(this.options.target).parents("p.content-mirror").first();
-        $el.on(
-            "input propertychange",
-            $.proxy(this.updateMirror, this, this.options.target)
+    async init() {
+        this.target = document.querySelector(this.options.target);
+        events.add_event_listener(
+            this.el,
+            "input",
+            "pat-content-mirror--update-mirror",
+            this.update_mirror.bind(this)
         );
-        $el.parents("form")
-            .first()
-            .on("reset", function () {
-                $el.val("");
-                $mirror.html($mirror.html());
-            });
-        $(".placeholder", this.options.target).text($el.attr("placeholder") || "");
-    },
 
-    updateMirror: function updateMirror(target, ev) {
-        const $el = $(ev.target);
-        const the_mirror = $(target);
-        the_mirror.text($el.val());
-        if (!$el.val().length) {
-            const placeholder = $el.attr("placeholder");
+        const form = this.el.form || this.el.closest("form");
+        events.add_event_listener(
+            form,
+            "reset",
+            `pat-content-mirror--reset--${this.uuid}`,
+            () => {
+                this.el.value = "";
+                this.el.dispatchEvent(events.input_event());
+            }
+        );
+
+        const placeholder = this.target.querySelector(".placeholder");
+        if (placeholder) {
+            placeholder.textContent = this.el.getAttribute("placeholder") || "";
+        }
+    }
+
+    update_mirror(ev) {
+        const el = ev.target;
+        const the_mirror = this.target;
+        const value = el.value;
+        the_mirror.textContent = value;
+        if (!value) {
+            const placeholder = this.el.getAttribute("placeholder");
             if (placeholder) {
-                the_mirror.html('<em class="placeholder">' + placeholder + "</em>");
+                the_mirror.innerHTML = `<em class="placeholder">${placeholder}</em>`;
             }
         }
-    },
-});
+    }
+}
+
+registry.register(Pattern);
+
+export default Pattern;
+export { Pattern };
